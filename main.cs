0ec2ffb795141311b3d4e516ab4f4ccc8111b363
ydvicencio <yvicencio@hawkmail.hccfl.edu>
2024-10-20 23:37:57 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,35 @@
+using System;
+//Unit Test 1
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Oh, well imagine /");
+    Console.WriteLine ("As I'm pacing the pews in a church corridor /");
+    Console.WriteLine ("And I can't help but to hear /");
+    Console.WriteLine ("No, I can't help but to hear an exchanging of words...");
+    Console.WriteLine ("");
+
+    //Unit test 2
+    Console.WriteLine ("Oh-oh-oh-oh-oh,");
+    Console.WriteLine ("Oh-oh-oh-oh,");
+    Console.WriteLine ("oh-oh-oh /");
+    Console.WriteLine ("Caught in a bad romance");
+    Console.WriteLine ("");
+
+    //Unit test 3
+    Console.WriteLine ("Coming out of my cage");
+    Console.WriteLine ("And I've been doing just fine");
+    Console.WriteLine ("Gotta gotta be down");
+    Console.WriteLine ("Because I want it all");
+    Console.WriteLine ("");
+
+    //Unit test 4
+    Console.WriteLine ("Now, this is a story all about how");
+    Console.WriteLine ("My life got flipped-turned upside down");
+    Console.WriteLine ("And I'd like to take a minute");
+    Console.WriteLine ("Just sit right there");
+    Console.WriteLine ("I'll tell you how I became the prince");
+    Console.WriteLine ("of a town called Bel-Air");
+    
+    
+  }
+}
